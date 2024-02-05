76c125a53626f43d5e9dba1e9146f873af946255
IJ-Jany <146128361+IJ-Jany@users.noreply.github.com>
2024-02-05 20:19:43 +0600

    2nd

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import './navbar.css'
+import React from 'react'
+import {Menuitems} from "./Menuitems"
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+  <nav className='navbaritems'>
+    <h1 className='nav-logo'> Trip py</h1>
+     <ul className='nav-menu'>
+      {Menuitems.map((item,index)=>{
+        return(
+          <li key={index}>
+            <a className={item.cName} href='/'>
+              <i className={item.icon}></i>
+              {item.title}
+            </a>
+            </li>
+          
+        )
+      })}
+      <button>Signup</button>
+     </ul>
+  </nav>
+  )
+}
+
+export default Navbar
+
